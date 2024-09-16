34f4dd590d7fcae4b9276c6f57ca60967be23180
Nobuhiro Oto <nobuhirooto@gmail.com>
2024-09-16 15:07:52 +0900

    16.9.2024

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,14 +1,38 @@
 import "./chatList.css"
+import {useState} from "react";
 
 const ChatList = () => {
+    const [addMode, setAddMode] = useState(false);
     return (
         <div className='chatList'>
             <div className="search">
                 <div className="searchBar">
                     <img src="/public/search.png" alt="search"/>
-                    <input type="text" placeholder="Search" />
+                    <input type="text" placeholder="Search"/>
                 </div>
-                <img src="/public/plus.png" alt="plus"/>
+                <img src={addMode ? "/public/minus.png" : "/public/plus.png"} className="add" alt="plus"
+                     onClick={() => setAddMode((prev) => !prev)}/>
+            </div>
+            <div className="item">
+                <img src="/public/avatar.png" alt="avatar"/>
+                <div className="texts">
+                    <span>Jane Doe</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="/public/avatar.png" alt="avatar"/>
+                <div className="texts">
+                    <span>Jane Doe</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="/public/avatar.png" alt="avatar"/>
+                <div className="texts">
+                    <span>Jane Doe</span>
+                    <p>Hello</p>
+                </div>
             </div>
         </div>
     )
