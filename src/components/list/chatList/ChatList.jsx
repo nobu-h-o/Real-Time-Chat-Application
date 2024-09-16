c937663b1d5d6aa08369dfd58766471dfd637dc7
Nobuhiro Oto <nobuhirooto@gmail.com>
2024-09-16 12:36:33 +0900

    15.9.2024

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -2,7 +2,15 @@ import "./chatList.css"
 
 const ChatList = () => {
     return (
-        <div className='chatList'>ChatList</div>
+        <div className='chatList'>
+            <div className="search">
+                <div className="searchBar">
+                    <img src="/public/search.png" alt="search"/>
+                    <input type="text" placeholder="Search" />
+                </div>
+                <img src="/public/plus.png" alt="plus"/>
+            </div>
+        </div>
     )
 }
 
